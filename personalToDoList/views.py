d7fb2e4b4fa21b4728a8cf43e5eb32d2355cde93
Hamidreza Alipour <hmr.alipour@gmail.com>
2024-08-03 16:56:48 +0330

    tasks page implemented

--- personalToDoList/views.py
+++ personalToDoList/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render, redirect
 
 from . import forms
 from django.contrib.auth import login, authenticate
-from personalToDoList.models import ToDoList
+from personalToDoList.models import ToDoList, Task
+
+
+def check_user_authentication(user):
+    pass
 
 
 def login_page(request):
@@ -26,21 +30,25 @@ def login_page(request):
 
 
 def to_do_lists_page(request):
-    # form = forms.ToDoListForm()
     user = request.user
     if not user.is_authenticated:
         return redirect("http://localhost:8000/admin")
     else:
         to_do_lists = ToDoList.objects.filter(user=user)
-        url = "http://localhost:8000"
         return render(request, 'to_do_list_page.html', context={'to_do_lists': to_do_lists})
 
 
 def tasks_page(request, list_id):
-    return render(request, 'base.html')
+    if not request.user.is_authenticated:
+        return redirect("http://localhost:8000/admin")
+    else:
+        tasks = Task.objects.filter(toDoList__id=list_id).order_by('due_date', '-is_priority')
+        return render(request, 'tasks.html', context={'tasks': tasks})
 
 
 def create_to_do_list(request):
+    if not request.user.is_authenticated:
+        return redirect("http://localhost:8000/admin")
     form = forms.ToDoListForm()
     if request.method == 'POST':
         form = forms.ToDoListForm(request.POST)
