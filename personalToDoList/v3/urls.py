529983dc8b875148956f6aadadef9b75f74594b0
Hamidreza Alipour <hmr.alipour@gmail.com>
2024-08-05 17:58:36 +0330

    creating tasks with generic implemented

--- personalToDoList/v3/urls.py
+++ personalToDoList/v3/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('to-do-lists/<int:list_id>/', personalToDoListView.TasksPageView.as_view(), name='tasks-page-v3'),
     path('create-task-with-uuid/<int:list_id>', personalToDoListView.CreateTaskWithUUIDView.as_view(), name='create-task-with-uuid-v3'),
     path('create-to-do-list/', personalToDoListView.CreateToDoListView.as_view(), name='create-to-do-list-v3'),
+    path('create-task/<int:list_id>', personalToDoListView.CreateTaskView.as_view(), name="create-task-v3"),
 ]
--- personalToDoList/v3/views.py
+++ personalToDoList/v3/views.py
@@ -24,51 +24,16 @@ class ToDoListsPageView(ListView):
         return context
 
 
-# class TasksPageView(LoginRequiredMixin, ListView, CreateView):
-#     model = Task
-#     template_name = 'tasks.html'
-#     form_class = forms.TokenForm
-#     list_id = None
-#     message = ''
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['tasks'] = self.object_list.filter(toDoList__id=self.kwargs['list_id']).order_by('due_date',
-#                                                                                                  '-is_priority')
-#         self.success_url = f"{DEFAULT_DOMAIN}v3/to-do-lists/{self.kwargs['list_id']}/"
-#         context['default_domain'] = DEFAULT_DOMAIN
-#         context['version'] = "v3"
-#         context['message'] = self.message
-#         context['form'] = self.get_form()
-#         self.list_id = self.kwargs['list_id']
-#         context['list_id'] = self.kwargs['list_id']
-#         return context
-#
-#     def form_valid(self, form):
-#         uuid = self.form_class.cleaned_data['uuid']
-#         try:
-#             token = Token.objects.get(uuid=uuid)
-#             to_do_list = ToDoList.objects.get(id=self.list_id)
-#             token.task.toDoLists.add(to_do_list)
-#             self.message = "task has been added"
-#         except ValidationError:
-#             self.message = "invalid token format"
-#         except Token.DoesNotExist:
-#             self.message = "token doesn't exist"
-#
-#         return super().form_valid(form)
-
-
 class TasksPageView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'tasks.html'
     message = ''
     list_id = None
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.filter(toDoLists__id=self.kwargs['list_id']).order_by('due_date', '-is_priority')
+        context['tasks'] = Task.objects.filter(toDoLists__id=self.kwargs['list_id']).order_by('due_date',
+                                                                                              '-is_priority')
         context['default_domain'] = DEFAULT_DOMAIN
         context['version'] = "v3"
         context['message'] = self.message
@@ -76,6 +41,7 @@ class TasksPageView(LoginRequiredMixin, ListView):
         self.list_id = self.kwargs['list_id']
         context['list_id'] = self.kwargs['list_id']
         return context
+
 
 class CreateTaskWithUUIDView(LoginRequiredMixin, FormView):
     form_class = forms.TokenForm
@@ -101,8 +67,6 @@ class CreateTaskWithUUIDView(LoginRequiredMixin, FormView):
         return f"{DEFAULT_DOMAIN}v3/to-do-lists/{self.kwargs['list_id']}/"
 
 
-
-
 class CreateToDoListView(LoginRequiredMixin, CreateView):
     model = ToDoList
     template_name = 'create-to-do-list.html'
@@ -116,4 +80,23 @@ class CreateToDoListView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CreateTaskView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'create-task.html'
+    form_class = forms.TaskForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        return context
+
+    def get_success_url(self):
+        return f"{DEFAULT_DOMAIN}v3/to-do-lists/{self.kwargs['list_id']}/"
+
+    def form_valid(self, form):
+        form.save()
+        form.instance.toDoLists.add(ToDoList.objects.get(id=self.kwargs['list_id']))
         return super().form_valid(form)
