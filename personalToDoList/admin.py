ce6d66a25d358279e0441ab59a6f712fdef2dcee
hamid <hmr.alipour@gmail.com>
2024-08-10 16:32:14 +0330

    rebasing fixed

--- personalToDoList/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from personalToDoList.models import ToDoList
-from personalToDoList.models import Task
-from personalToDoList.models import Token
-
-admin.site.register(Task)
-admin.site.register(ToDoList)
-admin.site.register(Token)
--- personalToDoList/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-urlpatterns = [
-    path('v1/', include('personalToDoList.v1.urls')),
-    path('v2/', include('personalToDoList.v2.urls')),
-    path('v3/', include('personalToDoList.v3.urls')),
-]
